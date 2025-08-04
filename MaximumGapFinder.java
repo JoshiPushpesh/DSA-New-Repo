a343d461f5b7afd45547c76034200fdc64862d3a
JoshiPushpesh <pushpeshjoshi48@gmail.com>
2025-08-04 20:23:05 +0530

    added MaximumGapFinder.java

---
+++ MaximumGapFinder.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+public class MaximumGapFinder {
+    
+    public static int maximumGap(int[] nums) {
+        if (nums.length < 2) return 0;
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+        }
+
+        if (min == max) return 0;
+
+        int n = nums.length;
+        int bucketSize = Math.max(1, (max - min) / (n - 1));
+        int bucketCount = ((max - min) / bucketSize) + 1;
+
+        int[] bucketMin = new int[bucketCount];
+        int[] bucketMax = new int[bucketCount];
+        boolean[] bucketUsed = new boolean[bucketCount];
+
+        Arrays.fill(bucketMin, Integer.MAX_VALUE);
+        Arrays.fill(bucketMax, Integer.MIN_VALUE);
+
+        for (int num : nums) {
+            int i = (num - min) / bucketSize;
+            bucketMin[i] = Math.min(bucketMin[i], num);
+            bucketMax[i] = Math.max(bucketMax[i], num);
+            bucketUsed[i] = true;
+        }
+
+        int maxGap = 0;
+        int prevMax = min;
+
+        for (int i = 0; i < bucketCount; i++) {
+            if (!bucketUsed[i]) continue;
+
+            maxGap = Math.max(maxGap, bucketMin[i] - prevMax);
+            prevMax = bucketMax[i];
+        }
+
+        return maxGap;
+    }
+
+    // Main method to test the function
+    public static void main(String[] args) {
+        int[] nums = {3, 6, 9, 1};
+        
+        int result = maximumGap(nums);
+        System.out.println("Maximum gap is: " + result);
+    }
+}
