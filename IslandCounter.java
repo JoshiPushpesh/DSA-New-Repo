7e03b84b3508f91a386133edce5c089686dd8526
JoshiPushpesh <pushpeshjoshi48@gmail.com>
2025-09-30 15:10:57 +0530

    added

---
+++ IslandCounter.java
@@ -0,0 +1,44 @@
+class IslandCounter {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) return 0;
+        
+        int count = 0;
+        int m = grid.length, n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        int m = grid.length, n = grid[0].length;
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == '0') {
+            return;
+        }
+
+        grid[i][j] = '0'; // mark visited
+
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+
+    // 🔹 Demo
+    public static void main(String[] args) {
+        IslandCounter solver = new IslandCounter();
+        char[][] grid = {
+            {'1','1','0','0','0'},
+            {'1','1','0','0','0'},
+            {'0','0','1','0','0'},
+            {'0','0','0','1','1'}
+        };
+        System.out.println(solver.numIslands(grid)); // Output: 3
+    }
+}
