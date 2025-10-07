2e3356bf29ad3b99d1d27b1ed6421845962dc6a6
JoshiPushpesh <pushpeshjoshi48@gmail.com>
2025-10-07 22:43:23 +0530

    added

---
+++ SubsetExplorer.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class SubsetExplorer {
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums, int start) {
+        result.add(new ArrayList<>(temp));
+        for (int i = start; i < nums.length; i++) {
+            temp.add(nums[i]);
+            backtrack(result, temp, nums, i + 1);
+            temp.remove(temp.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        SubsetExplorer obj = new SubsetExplorer();
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> subsets = obj.subsets(nums);
+        System.out.println("All subsets: " + subsets);
+    }
+}
