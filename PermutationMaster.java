42df7348da7769db65f3070bf7cdb78696ab957c
JoshiPushpesh <pushpeshjoshi48@gmail.com>
2025-10-11 15:47:29 +0530

    added

---
+++ PermutationMaster.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class PermutationMaster {
+
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, new boolean[nums.length], new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] nums, boolean[] used, List<Integer> current, List<List<Integer>> result) {
+        if (current.size() == nums.length) {
+            result.add(new ArrayList<>(current));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) continue;
+            used[i] = true;
+            current.add(nums[i]);
+            backtrack(nums, used, current, result);
+            current.remove(current.size() - 1);
+            used[i] = false;
+        }
+    }
+
+    public static void main(String[] args) {
+        PermutationMaster obj = new PermutationMaster();
+        int[] nums = {1, 2, 3};
+        List<List<Integer>> result = obj.permute(nums);
+        System.out.println("All permutations: " + result);
+    }
+}
