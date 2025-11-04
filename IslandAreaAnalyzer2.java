b7f1e4511a39610ad7b4b938f95aa953593269ca
JoshiPushpesh <pushpeshjoshi48@gmail.com>
2025-11-04 23:27:24 +0530

    adedd IslandAreaAnalyzer2.java

---
+++ IslandAreaAnalyzer2.java
@@ -0,0 +1,42 @@
+class IslandAreaAnalyzer2 {
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    int area = dfs(grid, i, j);
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0)
+            return 0;
+
+        grid[i][j] = 0; // mark visited
+        int area = 1;
+
+        // explore all 4 directions
+        area += dfs(grid, i + 1, j);
+        area += dfs(grid, i - 1, j);
+        area += dfs(grid, i, j + 1);
+        area += dfs(grid, i, j - 1);
+
+        return area;
+    }
+
+    public static void main(String[] args) {
+        IslandAreaAnalyzer2 analyzer = new IslandAreaAnalyzer2();
+        int[][] grid = {
+            {0,0,1,0,0,0,0,1,0,0,0,0,0},
+            {0,0,1,1,0,0,0,1,1,1,0,0,0},
+            {0,1,1,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0}
+        };
+        System.out.println("Max area of island: " + analyzer.maxAreaOfIsland(grid));
+    }
+}
