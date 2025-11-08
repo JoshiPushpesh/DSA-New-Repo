cec3a29e7c26cf8a5042838fda5675c4030c7727
JoshiPushpesh <pushpeshjoshi48@gmail.com>
2025-11-08 19:39:17 +0530

    added MaxSubarraySolverX.java

---
+++ MaxSubarraySolverX.java
@@ -0,0 +1,21 @@
+class MaxSubarraySolverX {
+
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int currentSum = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            // Either start new subarray at nums[i] or continue previous
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        MaxSubarraySolverX solver = new MaxSubarraySolverX();
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Maximum Subarray Sum: " + solver.maxSubArray(nums));
+    }
+}
