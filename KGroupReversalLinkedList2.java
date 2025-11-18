2f03266baf82fc45ae612d87d07ac47bb4091dc9
JoshiPushpesh <pushpeshjoshi48@gmail.com>
2025-11-18 21:08:41 +0530

    added KGroupReversalLinkedList2.java

---
+++ KGroupReversalLinkedList2.java
@@ -0,0 +1,77 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class KGroupReversalLinkedList2 {
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || k == 1) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prevGroup = dummy;
+        ListNode end = head;
+
+        while (true) {
+            int count = 0;
+            while (end != null && count < k) {
+                end = end.next;
+                count++;
+            }
+            if (count < k) break;
+
+            ListNode start = prevGroup.next;
+            ListNode nextGroup = end;
+
+            // Reverse k nodes
+            ListNode prev = nextGroup;
+            ListNode curr = start;
+
+            for (int i = 0; i < k; i++) {
+                ListNode temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+            }
+
+            prevGroup.next = prev;
+            prevGroup = start;
+        }
+
+        return dummy.next;
+    }
+
+    // Helper: Print linked list
+    public void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Create sample list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(4);
+        ListNode n5 = new ListNode(5);
+
+        n1.next = n2; n2.next = n3; n3.next = n4; n4.next = n5;
+
+        KGroupReversalLinkedList2 obj = new KGroupReversalLinkedList2();
+        System.out.println("Original List:");
+        obj.printList(n1);
+
+        ListNode newHead = obj.reverseKGroup(n1, 2);
+        System.out.println("List after reversing in groups of 2:");
+        obj.printList(newHead);
+    }
+}
