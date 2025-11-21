db31def7a6b5f2a27109b01ceb1cf3c0df7a1594
JoshiPushpesh <pushpeshjoshi48@gmail.com>
2025-11-21 22:50:18 +0530

    added PalindromeFinderX.java

---
+++ PalindromeFinderX.java
@@ -0,0 +1,32 @@
+class PalindromeFinderX {
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandFromCenter(s, i, i);       // Odd-length palindrome
+            int len2 = expandFromCenter(s, i, i + 1);   // Even-length palindrome
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1; // Length of palindrome
+    }
+
+    public static void main(String[] args) {
+        PalindromeFinderX finder = new PalindromeFinderX();
+        String s = "babad";
+        System.out.println("Longest Palindromic Substring: " + finder.longestPalindrome(s));
+    }
+}
